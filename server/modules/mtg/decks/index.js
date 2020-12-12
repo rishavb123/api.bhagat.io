9913b27fe9a01b0febf9005e17fa03a65841dff7
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-12-12 12:11:02 -0500

    added get deck list module

---
+++ server/modules/mtg/decks/index.js
@@ -0,0 +1,34 @@
+const rp = require('request-promise');
+const $ = require('cheerio');
+
+function getDeckList(url) {
+    return rp(url)
+        .then((html) => {
+            const get_prop = (prop) => $(`.deck-list-entry > .deck-list-entry-${prop} > *`, html);
+            const get_text = (el) => el.firstChild.data.trim();
+            
+            const counts = get_prop('count');
+            const names = get_prop('name');
+
+            const deckList = [];
+
+            for (let i = 0; i < counts.length; i++) {
+                deckList.push({
+                    link: counts[i].attribs.href,
+                    count: parseInt(get_text(counts[i])),
+                    name: get_text(names[i])
+                });                
+            }
+            return deckList;
+        })
+        .catch(console.log);
+}
+
+function getStringDeckList(url) {
+    return getDeckList(url).then((data) => data.map((el) => el.count + " " + el.name));
+}
+
+module.exports = {
+    getDeckList,
+    getStringDeckList
+}
