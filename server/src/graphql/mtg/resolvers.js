de6a44028834213537fc906aa3de45423016d340
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-12-15 17:37:09 -0500

    returning cards in the deck

--- server/src/graphql/mtg/resolvers.js
+++ server/src/graphql/mtg/resolvers.js
@@ -1,7 +1,11 @@
+import { getDeckList } from "../../modules/mtg/decks";
+
 const url = 'https://scryfall.com/@rishavb123/decks/2e961cff-f599-4253-952e-ab66874f4b89'
 
 export default {
     Deck: {
-        
+        cards: async (url) => {
+            return await getDeckList(url);
+        }
     },
 };
--- server/src/graphql/mtg/schema.js
+++ server/src/graphql/mtg/schema.js
@@ -3,5 +3,11 @@ import { gql } from 'apollo-server-express';
 export default gql`
     type Deck {
         url: String!
+        cards: [Card]!
     } 
+    type Card {
+        name: String!
+        count: Int,
+        link: String
+    }
 `;
