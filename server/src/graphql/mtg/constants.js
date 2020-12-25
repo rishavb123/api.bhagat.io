06cae7a2fa9da8c6c6291194c356beb2f94b4737
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2020-12-24 19:54:26 -0500

    updated deck urls

--- server/src/graphql/mtg/constants.js
+++ server/src/graphql/mtg/constants.js
@@ -1,30 +1,30 @@
 export const myDecks = [
     {
         searchTerm: 'Araumi of the Dead Tide',
-        url: 'https://scryfall.com/@rishavb123/decks/2e961cff-f599-4253-952e-ab66874f4b89',
+        url: 'https://scryfall.com/@rishavb123/decks/07a46f56-1b5e-4a31-ad82-963db2230f9c',
     },
     {
         searchTerm: 'Jodah, Archmage Eternal',
-        url: 'https://scryfall.com/@rishavb123/decks/fe93c7a5-a34b-4344-a38c-01f77fa19c79',
+        url: 'https://scryfall.com/@rishavb123/decks/d41ae7d5-296c-4d78-8e64-27b1bff504db',
     },
     {
         searchTerm: 'Aminatou, the Fateshifter',
-        url: 'https://scryfall.com/@rishavb123/decks/dfe3661b-7c1c-4c21-9e38-8b323bcabc53',
+        url: 'https://scryfall.com/@rishavb123/decks/c438cc12-7133-427c-9ddb-cf7f10df7694',
     },
     {
         searchTerm: 'Golos, Tireless Pilgrim',
-        url: 'https://scryfall.com/@rishavb123/decks/e55b7a05-128b-4a8d-bebd-a91aaabddfd2',
+        url: 'https://scryfall.com/@rishavb123/decks/10d5ceb9-d255-49d4-8690-b37087b76ddb',
     },
     {
         searchTerm: 'Kruphix, God of Horizons',
-        url: 'https://scryfall.com/@rishavb123/decks/6757c4c4-a522-4d14-a0c1-1c6fe156f7d0',
+        url: 'https://scryfall.com/@rishavb123/decks/03079b3d-faf9-4fcc-a246-f6c7b7d644cc',
     },
     {
         searchTerm: 'Tatyova, Benthic Druid',
-        url: 'https://scryfall.com/@rishavb123/decks/8e08c4bd-dc0e-4f2a-8e00-58d055dd5a05',
+        url: 'https://scryfall.com/@rishavb123/decks/b1ba28ed-93bb-4ef4-ac72-4c25ddebabde',
     },
     {
         searchTerm: 'Edric, Spymaster of Trest',
-        url: 'https://scryfall.com/@rishavb123/decks/80227bb8-353a-439b-902a-f1f75119a5db',
+        url: 'https://scryfall.com/@rishavb123/decks/c9a3989d-6611-4516-b4b7-0d6e0e79221c',
     },
 ];
