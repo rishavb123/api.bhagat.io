8b5e40b07e95015634ef1f4d688e0c0735e3133e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-09 12:53:36 -0400

    added more gql queries for different decks

--- scripts/mtg/utils.py
+++ scripts/mtg/utils.py
@@ -31,7 +31,82 @@ superfriends_proxies = gql(
 """
 )
 
-query = rionya
+araumi_proxies = gql(
+    """
+
+    query {
+        deck(url: "https://scryfall.com/@rishavb123/decks/c39a4c33-21f7-4cd9-b9ab-ae07e3fa742d?as=list&with=usd") {
+            cards {
+                name,
+                scryfallApiData
+            }
+        }
+    }
+
+"""
+)
+
+tatyova_proxies = gql(
+    """
+
+    query {
+        deck(url: "https://scryfall.com/@rishavb123/decks/840ff970-b653-4d10-8510-ad8909d0c813?as=list&with=usd") {
+            cards {
+                name,
+                scryfallApiData
+            }
+        }
+    }
+
+"""
+)
+
+kruphix_proxies = gql(
+    """
+
+    query {
+        deck(url: "https://scryfall.com/@rishavb123/decks/bd903d18-a2f8-426a-9891-e6e803782c12?as=list&with=usd") {
+            cards {
+                name,
+                scryfallApiData
+            }
+        }
+    }
+
+"""
+)
+
+edric_proxies = gql(
+    """
+
+    query {
+        deck(url: "https://scryfall.com/@rishavb123/decks/64b88456-104d-4069-91aa-41e9aad03f71?as=list&with=usd") {
+            cards {
+                name,
+                scryfallApiData
+            }
+        }
+    }
+
+"""
+)
+
+jodah_proxies = gql(
+    """
+
+    query {
+        deck(url: "https://scryfall.com/@rishavb123/decks/c5451926-3299-4cdf-b145-bf455630133b?as=list&with=usd") {
+            cards {
+                name,
+                scryfallApiData
+            }
+        }
+    }
+
+"""
+)
+
+query = araumi_proxies
 
 l, h = float(sys.argv[1] if len(sys.argv) > 1 else 0), float(sys.argv[2] if len(sys.argv) > 2 else 100)
 
