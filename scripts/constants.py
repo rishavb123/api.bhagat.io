3afea052728fa9d82acb102b4f83c7c04469a7ba
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-15 00:53:26 -0400

    added dev and prod environments

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,2 +1,6 @@
-endpoint = "http://localhost:3000"
-graphql_endpoint = f"{endpoint}/graphql"
+from collections import namedtuple
+
+Endpoint = namedtuple("Endpoint", "dev prod")
+
+endpoint = Endpoint("http://localhost:3000", "https://bhagat-api.herokuapp.com")
+graphql_endpoint = Endpoint(f"{endpoint.dev}/graphql", f"{endpoint.prod}/graphql")
