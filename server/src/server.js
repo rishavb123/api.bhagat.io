88b208184dfda05b092ea60af480974242110099
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-16 16:10:37 -0400

    counting total ram usage

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/usage', (req, res) => {
-    const memUsed = process.memoryUsage().heapUsed / 1024 / 1024;
+    const memUsed = (process.memoryUsage().heapUsed + process.memoryUsage().rss + process.memoryUsage().external) / 1024 / 1024;
     res.json({
         memory: `${memUsed} mb`
     });
