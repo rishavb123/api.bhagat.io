af35c46cbef061fa95135973eb98011be29dd283
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 15:46:22 -0400

    wrote cron to update mongo db with edh decks

--- server/src/crons/mtg/update_decks/index.js
+++ server/src/crons/mtg/update_decks/index.js
@@ -1,56 +1,65 @@
 import { MongoClient } from 'mongodb';
+import { getScryfallApiData } from '../../../modules/mtg/cards';
+import { getDeckListInfo, getDeckListsFromUser } from '../../../modules/mtg/decks';
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}` +
     `@${process.env.MONGODB_CLUSTER}.vjvrg.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
 
 export default [
     {
-        expression: '*/10 * * * * *',
+        expression: '0 * * * *',
         task: async () => {
+            const decks = await getDeckListsFromUser("rishavb123", false);
+            for (const deck of decks) {
+                const moreInfo = await getDeckListInfo(deck.url, false);
+                deck.cards = moreInfo.cards;
+                deck.description = moreInfo.description;
+                if (deck.name.includes('EDH Commander Deck')) {
+                    deck.commander = {
+                        name: deck.name.split(' EDH Commander Deck')[0],
+                    };
+                }
+                else if (deck.deckType === 'Commander / EDH') {
+                    deck.commander = {
+                        name: moreInfo.cards[0].name
+                    };
+                } else {
+                    continue;
+                }
+                let data = await getScryfallApiData(deck.commander.name);
+                if (data.card_faces) {
+                    let found = false;
+                    for (const face of data.card_faces) {
+                        if (face.name === deck.commander.name) {
+                            data = face;
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                    data = data.card_faces[0];
+                }
+                deck.commander.image_url = data.image_uris.png;
+                console.log(deck.commander.name);
+            }
             
-            console.log("hello");
-        },
-        disabled: false
-    },
-    {
-        expression: '*/30 * * * * *',
-        task: async () => {
+            const client = new MongoClient(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
             try {
-                const client = new MongoClient(uri, {
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true,
-                });
                 await client.connect();
                 const db = await client.db('bhagat-db').collection('mtg-edh-decks');
 
-                const cursor = await db.find();
-                await cursor.forEach(console.log);
-
-                const val = {
-                    name: 'Golos, Tireless Pilgrim EDH Commander Deck - My Favorite',
-                    commander: {
-                        name: 'Golos, Tireless Pilgrim',
-                        image_uri: 'https://c1.scryfall.com/file/scryfall-cards/png/front' +
-                            '/ 1 / f / 1fa48620- 4c3d- 4f75-be1f - c12c4aa59f51.png ? 1592517637',
-                    },
-                    url: 'https://www.moxfield.com/decks/XMdFc9O33kG_QBtyC6bWNw2',
-                    description: 'Blink and you will miss it!',
-                };
-                const filter = {
-                    url: val.url,
-                };
-
-                await db.replaceOne(filter, val, {
-                    upsert: true,
-                });
-
-                console.log('Connected successfully to server');
+                await db.drop();
+                await db.insertMany(decks);
             } catch (e) {
                 console.log(e);
             } finally {
                 await client.close();
             }
+
         },
-        disabled: true,
+        disabled: false
     },
 ];
