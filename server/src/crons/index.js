455f999ae9f25a1013b78cb1b1c42d01608e2ca1
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 00:01:52 -0400

    commented unused lines

--- server/src/crons/index.js
+++ server/src/crons/index.js
@@ -1,28 +1,31 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}` +
-    `@${process.env.MONGODB_CLUSTER}.vjvrg.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}` +
+//     `@${process.env.MONGODB_CLUSTER}.vjvrg.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
 
 export default [
     // {
     //     expression: '* * * * * *',
-    //     task: () => console.log("Running every second!"),
-    //     options: {}
+    //     task: () => console.log('Running every second!'),
+    //     options: {},
     // },
     // {
     //     expression: '*/10 * * * * *',
     //     task: async () => {
     //         try {
-    //             const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    //             const client = new MongoClient(uri, {
+    //                 useNewUrlParser: true,
+    //                 useUnifiedTopology: true,
+    //             });
     //             await client.connect();
-    //             await client.db("bhagat-db").command({ ping: 1 });
-    //             // console.log("Connected successfully to server");
+    //             await client.db('bhagat-db').command({ ping: 1 });
+    //             console.log('Connected successfully to server');
     //         } catch (e) {
     //             console.log(e);
     //         } finally {
     //             await client.close();
     //         }
     //     },
-    //     options: {}
-    // }
+    //     options: {},
+    // },
 ];
