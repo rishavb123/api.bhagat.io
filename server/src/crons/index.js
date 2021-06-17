0a020e5eb80be4082cbed0a6ac98e11a195a1614
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 15:58:29 -0400

    deleted unused cron

--- server/src/crons/index.js
+++ server/src/crons/index.js
@@ -2,9 +2,4 @@ import mtgCrons from './mtg';
 
 export default [
     ...mtgCrons,
-    {
-        expression: '* * * * * *',
-        task: () => console.log('Running every second!'),
-        disabled: true,
-    },
 ];
