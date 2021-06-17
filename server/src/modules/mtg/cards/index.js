d1422fffb7640410be5773a3c9c318c38abf23b7
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 09:03:38 -0400

    added delay to scryfall api calls

--- server/src/modules/mtg/cards/index.js
+++ server/src/modules/mtg/cards/index.js
@@ -1,11 +1,14 @@
 import got from 'got/dist/source';
 import cache from 'memory-cache';
 
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 export async function getScryfallApiData(name, caching=true) {
     let result = cache.get(`scryfall-card-data-${name}`);
     if (result) {
         return result;
     }
+    await delay(100);
     result = JSON.parse((await got('https://api.scryfall.com/cards/named?exact=' + name)).body);
     if (caching) {
         cache.put(`scryfall-card-data-${name}`, result, 10000);
