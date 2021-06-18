3728254331feb604e9ab02845b574a180ba1180c
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 20:29:56 -0400

    added return functionality

--- server/src/modules/db/index.js
+++ server/src/modules/db/index.js
@@ -11,12 +11,14 @@ export function makeClient() {
 
 export async function wrapFunctionality(func) {
     const client = makeClient();
+    let returnVal;
     try {
         await client.connect();
-        await func(client);
+        returnVal = await func(client);
     } catch (e) {
         console.log(e);
     } finally {
         await client.close()
     }
+    return returnVal;
 }
