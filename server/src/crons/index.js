6e13a2be2e9bebd6c290a5cc3129d18b5a651f39
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 23:14:13 -0400

    added good morning job

--- server/src/crons/index.js
+++ server/src/crons/index.js
@@ -1,5 +1,15 @@
 import mtgCrons from './mtg';
 
 export default [
+    {
+        name: 'good_morning',
+        expression: '30 7 * * *',
+        task: async () => {
+            const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+            await delay(5000);
+            console.log("Good Morning!");
+        },
+        disabled: true
+    },
     ...mtgCrons,
 ];
