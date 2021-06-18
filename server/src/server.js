c564a19f751b6b124668b2471f32f492f0de4ea6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 20:11:39 -0400

    setup cors and made minor updates

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
-import cron from 'node-cron';
+import cors from 'cors';
 import { ApolloServerPluginInlineTrace } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-express';
+import cron from 'node-cron';
 
 import { schema, resolvers } from './graphql';
 import jobs from './crons';
-import setupApp from './routes';
+import addRoutes from './routes';
 
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const server = new ApolloServer({
     typeDefs: schema,
@@ -15,10 +20,9 @@ const server = new ApolloServer({
     introspection: true,
     plugins: [ApolloServerPluginInlineTrace()],
 });
-const app = express();
 server.applyMiddleware({ app });
 
-setupApp(app);
+addRoutes(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
