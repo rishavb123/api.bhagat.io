11b0a010c9c214b14525d4eaaae6d87c1dc08e2d
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 21:06:15 -0400

    finished db get route

--- server/src/routes/db/index.js
+++ server/src/routes/db/index.js
@@ -3,12 +3,15 @@ import { wrapFunctionality } from '../../modules/db';
 export default function (app) {
     app.get('/db', async (req, res) => {
         const params = req.body;
+        console.log(params);
         await wrapFunctionality(async (client) => {
             const db = await client.db(params.db || 'bhagat-db');
             const collection = await db.collection(params.collection || 'mtg-edh-decks');
-            let cursor = await collection.find(params.query || {}, params.options || {});
+            const cursor = await collection.find(params.query || {}, params.options || {});
+            const docs = await cursor.toArray();
             res.json({
-                documents: cursor.toArray(),
+                documents: docs,
+                count: docs.length,
             });
         });
     });
