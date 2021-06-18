5e8bb94a0b992774fe0565c71f5f125728c0387a
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-17 23:11:52 -0400

    added routes to get job by name and to run job

--- server/src/routes/jobs/index.js
+++ server/src/routes/jobs/index.js
@@ -1,7 +1,7 @@
 import jobs from '../../crons';
 
 export default function (app) {
-    app.get('/jobs', async (req, res) => {
+    app.get('/jobs', (req, res) => {
         res.json({
             jobs: jobs.map((job) => ({
                 name: job.name,
@@ -11,4 +11,35 @@ export default function (app) {
             }))
         });
     });
+
+    app.get('/jobs/:jobName', (req, res) => {
+        res.json({
+            job: jobs.find((job) => job.name == req.params.jobName)
+        });
+    });
+
+    app.get('/jobs/:jobName/run', async (req, res) => {
+        const wait = req.query.wait || 'false';
+        const job = jobs.find((job) => job.name == req.params.jobName);
+        if (job) {
+            if (wait.toLowerCase() === 'true') {
+                await job.task();
+                res.json({
+                    status: 0,
+                    msg: 'Finished running job'
+                });
+            } else {
+                job.task();
+                res.json({
+                    status: 0,
+                    msg: 'Started job in the background'
+                });
+            }
+        } else {
+            res.json({
+                status: 1,
+                msg: 'Job not found. See /jobs to see all jobs. '
+            });
+        }
+    })
 }
