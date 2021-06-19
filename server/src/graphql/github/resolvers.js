a3517a66bd49d8019edbd26e299092c9cc05d834
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-19 17:50:51 -0400

    wrote graphql wrapper for github module

---
+++ server/src/graphql/github/resolvers.js
@@ -0,0 +1,52 @@
+import { getAdditionalInfo, getLanguages } from '../../modules/github';
+import { USER } from '../../modules/github/constants';
+
+export default {
+    Repo: {
+        owner: ({ owner }) => {
+            return owner.login;
+        },
+        info: async ({ name }) => {
+            const info = await getAdditionalInfo(name);
+            return {
+                ...info,
+                name
+            };
+        },
+        createdDate: ({ created_at }) => {
+            return created_at;
+        },
+        lastUpdated: ({ updated_at }) => {
+            return updated_at;
+        },
+        languages: async ({ languages_url }) => {
+            return await getLanguages(languages_url);
+        },
+    },
+    ExtraRepoInfo: {
+        links: ({ name, links, ignoreSource, ignoreDownload }) => {
+            const newLinks = [...links];
+            let sourceExists = false;
+            let downloadExists = false;
+            for (const link of links) {
+                if (link.name.toLowerCase() === 'source')
+                    sourceExists = true;
+                if (link.name.toLowerCase() === 'download')
+                    downloadExists = true;
+            }
+            if (!ignoreSource && !sourceExists) {
+                newLinks.push({
+                    name: 'Source',
+                    url: `https://github.com/${USER}/${name},`
+                })
+            }
+            if (!ignoreDownload && !downloadExists) {
+                newLinks.push({
+                    name: 'Download',
+                    url: `https://github.com/${USER}/${name}/archive/refs/heads/master.zip`,
+                });
+            }
+            return newLinks;
+        },
+    }
+}
---
+++ server/src/graphql/github/schema.js
@@ -0,0 +1,25 @@
+import { gql } from 'apollo-server-express';
+
+export default gql`
+    type Repo {
+        name: String!
+        owner: String!
+        info: ExtraRepoInfo!
+        description: String
+        createdDate: String!
+        lastUpdated: String!
+        languages: [Language]!
+    }
+    type ExtraRepoInfo {
+        imageUrl: String!
+        links: [Links]!
+    }
+    type Language {
+        name: String!
+        percent: Float
+    }
+    type Link {
+        name: String!
+        url: String!
+    }
+`;
