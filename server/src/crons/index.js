f954bf6413984879c6326415da02de77175f398f
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-20 15:49:42 -0400

    added another TODO

--- server/src/crons/index.js
+++ server/src/crons/index.js
@@ -2,6 +2,7 @@ import mtgCrons from './mtg';
 
 // TODO: Add cron to get data from github and store it into mongo db
 // (use local executable schema)
+// TODO: only run crons in production environment unless runOnDev: true
 
 export default [
     {
