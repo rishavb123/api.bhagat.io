b96a290680a0e9dfdf7f1cfbd65dfa784aa34e27
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-06-21 08:16:51 -0400

    added todos

--- server/src/graphql/index.js
+++ server/src/graphql/index.js
@@ -2,4 +2,5 @@ import schema from './schema';
 import resolvers from './resolvers';
 
 // TODO: add linkedIn section and automate website with it
+// TODO: add discord module to allow people to message me from website
 export { schema, resolvers };
--- server/src/modules/db/constants.js
+++ server/src/modules/db/constants.js
@@ -1,2 +1,4 @@
 export const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}` +
     `@${process.env.MONGODB_CLUSTER}.vjvrg.mongodb.net/${process.env.MONGODB_DBNAME}?retryWrites=true&w=majority`;
+
+// TODO: move the mongo db constants like cluster and dbname to this file
