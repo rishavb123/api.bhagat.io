b44d02558d593369cf587cf5ce35c010b63d52dd
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-07-03 15:23:41 -0400

    removed log

--- server/src/graphql/local.js
+++ server/src/graphql/local.js
@@ -3,6 +3,5 @@ import { graphql } from '../../node_modules/graphql';
 import schema from '.';
 
 export async function queryGraphQL(query, variables = {}) {
-    console.log(variables);
     return await graphql({ schema, source: query, variableValues: variables });
 }
