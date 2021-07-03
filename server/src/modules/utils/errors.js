2583d5d605ef29d25971d513ff7ab7b2a9d004b1
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-07-03 15:39:41 -0400

    fixed handler to return function

--- server/src/modules/utils/errors.js
+++ server/src/modules/utils/errors.js
@@ -1,10 +1,12 @@
-export async function wrapWithErrorHandling(func, errorCallback=null) {
-    try {
-        return await func();
-    } catch (e) {
-        console.log(`Error in ${func.name}:`, e);
-        if (errorCallback) {
-            errorCallback(e);
+export function wrapWithErrorHandling(func, errorCallback = null) {
+    return async (...args) => {
+        try {
+            return await func(...args);
+        } catch (e) {
+            console.log(`Error in ${func.name}:`, e);
+            if (errorCallback) {
+                errorCallback(e);
+            }
         }
-    }
+    };
 }
