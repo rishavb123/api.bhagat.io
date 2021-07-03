fb76d98f408f377642c45dacbfd69da4e08f2170
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-07-02 23:57:53 -0400

    added priority to job gql call

--- server/src/jobs/github/index.js
+++ server/src/jobs/github/index.js
@@ -17,6 +17,7 @@ export default [
                                 url
                             }
                             imageUrl
+                            priority
                         }
                         languages {
                             name
