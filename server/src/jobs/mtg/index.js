b26b28d7af04c3515c4ea3d81a17717567aa72f0
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-07-04 12:57:07 -0400

    switched back to called module methods

--- server/src/jobs/mtg/index.js
+++ server/src/jobs/mtg/index.js
@@ -1,77 +1,67 @@
+import { getScryfallApiData } from '../../modules/mtg/cards';
+import { getDeckListInfo, getDeckListsFromUser } from '../../modules/mtg/decks';
 import { wrapWithDbClient } from '../../modules/db';
-import { queryGraphQL } from '../../graphql/local';
+import { MOXFIELD_USER } from '../../modules/mtg/constants';
 
 export default [
     {
         name: 'update_decks',
         expression: '5 */2 * * *',
         task: async () => {
-            const query = `
-                query getDecks($caching: Boolean!) {
-                    mydecks(caching: $caching) {
-                        url
-                        name
-                        deckType
-                        cards {
-                            name
-                            count
-                            link
-                        }
-                        description
-                        commander {
-                            name
-                            scryfallApiData
-                        }
-                    }
-                }
-            `;
-            const variables = {
-                caching: false,
+            const decks = await getDeckListsFromUser(MOXFIELD_USER, false);
+            while (decks.length == 0) {
+                await getDeckListsFromUser(MOXFIELD_USER, false);
             }
-            const result = await queryGraphQL(query, variables);
-            if (!result.errors) {
-                const decks = result.data.mydecks;
-                console.log(`Read in ${decks.length} decks from moxfield. Processing data . . .`);
-                for (const deck of decks) {
-                    let data = deck.commander.scryfallApiData;
+            console.log(`Read in ${decks.length} decks from moxfield`);
+            for (const deck of decks) {
+                const moreInfo = await getDeckListInfo(deck.url, false);
+                deck.cards = moreInfo.cards;
+                deck.description = moreInfo.description;
+                if (deck.name.includes('EDH Commander Deck')) {
                     deck.commander = {
-                        name: deck.commander.name,
+                        name: deck.name.split(' EDH Commander Deck')[0],
                     };
-                    deck.commander.color_identity = data.color_identity;
-                    deck.commander.mana_value = data.cmc;
-                    if (data.card_faces) {
-                        let found = false;
-                        for (const face of data.card_faces) {
-                            if (face.name === deck.commander.name) {
-                                data = face;
-                                found = true;
-                                break;
-                            }
-                        }
-                        if (!found) {
-                            data = data.card_faces[0];
+                } else if (deck.deckType === 'Commander / EDH') {
+                    deck.commander = {
+                        name: moreInfo.cards[0].name,
+                    };
+                } else {
+                    continue;
+                }
+                let data = await getScryfallApiData(deck.commander.name);
+                deck.commander.color_identity = data.color_identity;
+                deck.commander.mana_value = data.cmc;
+                if (data.card_faces) {
+                    let found = false;
+                    for (const face of data.card_faces) {
+                        if (face.name === deck.commander.name) {
+                            data = face;
+                            found = true;
+                            break;
                         }
                     }
-                    deck.commander.image_url = data.image_uris.png;
-                    deck.commander.art_crop = data.image_uris.art_crop;
+                    if (!found) {
+                        data = data.card_faces[0];
+                    }
                 }
-                if (decks.length > 0) {
-                    console.log('Pushing decks to MongoDB . . .');
-                    await wrapWithDbClient(async (client) => {
-                        const db = await client.db('bhagat-db');
-                        const collection = await db.collection('mtg-edh-decks');
+                deck.commander.image_url = data.image_uris.png;
+                deck.commander.art_crop = data.image_uris.art_crop;
+            }
+            if (decks.length > 0) {
+                console.log('Pushing decks to MongoDB . . .');
+                await wrapWithDbClient(async (client) => {
+                    const db = await client.db('bhagat-db');
+                    const collection = await db.collection('mtg-edh-decks');
 
-                        if ((await collection.countDocuments()) !== 0) {
-                            await collection.drop();
-                        }
-                        await collection.insertMany(decks);
-                    });
-                } else {
-                    console.log('0 decks found! Not pushing to MongoDB');
-                }
+                    if ((await collection.countDocuments()) !== 0) {
+                        await collection.drop();
+                    }
+                    await collection.insertMany(decks);
+                });
             } else {
-                throw new Error(result.errors[0].message);
+                console.log('0 decks found! Not pushing to MongoDB');
             }
         },
+        disabled: false,
     },
 ];
