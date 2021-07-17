d7fdaa98ddaa62c8774efe20d37c12fe6acb1e54
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-07-17 13:32:22 -0400

    added hook to github repos in read_repos job

--- server/src/jobs/github/index.js
+++ server/src/jobs/github/index.js
@@ -1,5 +1,7 @@
 import { queryGraphQL } from '../../graphql/local';
 import { wrapWithDbClient } from '../../modules/db';
+import { addHook } from '../../modules/github';
+import { USER } from '../../modules/github/constants';
 
 export default [
     {
@@ -34,9 +36,24 @@ export default [
                 forceNoDb: true,
             };
             const resp = await queryGraphQL(query, variables);
+            console.log('Read in data using Github API.');
 
             if (resp.data && resp.data.repos && resp.data.repos.length > 0) {
-                console.log('Read in data using Github API. Now pushing to MongoDB . . .');
+                console.log('Adding discord webhooks to any github repositories that do not already have it . . .');
+                for (const repo of resp.data.repos) {
+                    await addHook({
+                        'name': 'web',
+                        'active': true,
+                        'events': [
+                            '*',
+                        ],
+                        'config': {
+                            'url': process.env.GH_DISCORD_UPDATE_URL,
+                            'content_type': 'json',
+                        },
+                    }, `https://api.github.com/repos/${USER}/${repo.name}/hooks`);
+                }
+                console.log('Pushing to MongoDB . . .');
                 await wrapWithDbClient(async (client) => {
                     const db = await client.db('bhagat-db');
                     const collection = await db.collection('gh-repos');
