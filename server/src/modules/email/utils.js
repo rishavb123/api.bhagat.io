a7a3ab82ed4b63beb33d84de9129f9906820241b
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-08-01 15:39:37 -0400

    added email validation

--- server/src/modules/email/utils.js
+++ server/src/modules/email/utils.js
@@ -1,4 +1,8 @@
 export function makeComposeUrl(email, subject = '', body = '', bcc = '', cc = '') {
-    cc = 'test@bhagat.io';
-    return `https://mail.google.com/mail/?fs=1&to=${email}&su=${subject}&body=${body}&bcc=${bcc}&cc=${cc}&tf=cm`
+    const url = `https://mail.google.com/mail/?fs=1&to=${email}&su=${subject}&body=${body}&bcc=${bcc}&cc=${cc}&tf=cm`;
+    return url.replace(/ /g, '%20').replace(/\n/g, '%0A');
+}
+
+export function validateEmail(email) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
 }
--- server/src/routes/contact/index.js
+++ server/src/routes/contact/index.js
@@ -1,17 +1,24 @@
 import { sendMessageEmbed } from "../../modules/discord";
 import { API_MESSAGES_CHANNEL_ID, USER_ID } from "../../modules/discord/contants";
-import { makeComposeUrl } from "../../modules/email/utils";
+import { makeComposeUrl, validateEmail } from "../../modules/email/utils";
 import { addRoute } from "../utils";
 
 export default function (app) {
     addRoute('/contact/discord', app.post.bind(app), async (req, res) => {
         const { name, message, returnAddress } = req.body;
-        const composeUrl = makeComposeUrl(returnAddress);
+        const composedUrl = makeComposeUrl(returnAddress, '', `Hey ${name}, \n\n\n\nRegards,\nRishav Bhagat`);
+
+        if (!validateEmail(returnAddress)) {
+            res.json({
+                status: 1,
+                msg: 'Invalid Email Address',
+            })
+        }
 
         await sendMessageEmbed(
             API_MESSAGES_CHANNEL_ID,
             `New Message from ${name}!`,
-            `<@${USER_ID}> \n${message}\nReach me at [${returnAddress}](${composeUrl})!`,
+            `<@${USER_ID}> \n${message}\nReach me at [${returnAddress}](${composedUrl})!`,
         );
 
         res.json({
