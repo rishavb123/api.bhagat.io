566c584c6e227618855a29b5873cfdb407456627
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-09-08 00:17:17 -0400

    working on sign up through bot

--- server/src/modules/gtmtg/discord.js
+++ server/src/modules/gtmtg/discord.js
@@ -1,9 +1,11 @@
 import Discord from 'discord.js';
 import { delay } from '../utils/misc';
 import { SANDBOX_CHANNEL_ID, WELCOME_CHANNEL_ID, ROLES_CHANNEL_ID, INTRODUCTIONS_CHANNEL_ID, ANNOUNCEMENTS_CHANNEL_ID } from './constants';
+import { addPerson } from './roster';
 
 
 const client = new Discord.Client();
+const botState = {};
 
 export async function startGTMTGDiscordBot() {
     await client.login(process.env.GTMTG_DISCORD_BOT_TOKEN);
@@ -18,6 +20,44 @@ export async function startGTMTGDiscordBot() {
             Check <#${ANNOUNCEMENTS_CHANNEL_ID}> or our website https://mtg.bhagat.io/ for any upcoming events. Feel free to poke around various channels and join us on Fridays for our weekly game nights!`
         );
     });
+    client.on('message', async (message) => {
+        if (message.channel.type == 'dm' && !message.author.bot) {
+            const content = message.content;
+            const userId = message.author.id;
+            const state = botState[userId];
+            if (state && state.process) {
+                switch (state.process) {
+                    case "sign_up":
+                        state.info.push(message.content.trim());
+                        await message.reply(state.dialogs[state.step - 1])
+                        state.step++;
+                        if (state.step == 4) {
+                            const result = await addPerson(state.info[0], state.info[1], state.info[2], message.author.username, userId);
+                            botState[userId] = {};
+                            if (!result)
+                                message.reply('It looks like you are already in our database!');
+                        }
+                        break;
+                }
+            } else if (content.toLowerCase().match(/sign up/g)) {
+                await message.reply('Starting sign up process . . .');
+                await message.reply('Please type your first name');
+                botState[userId] = {
+                    process: 'sign_up',
+                    step: 1,
+                    info: [],
+                    dialogs: [
+                        'Thanks! Please type your last name',
+                        'What is your GT user name? (ex: gburdell3)',
+                        'Awesome! Thanks for the info! I\'ll add you into our database now!',
+                    ],
+                };
+
+            } else {
+
+            }
+        }
+     });
 }
 
 // TODO: modularize this code with the discord module
---
+++ server/src/modules/gtmtg/roster.js
@@ -0,0 +1,22 @@
+import { wrapWithDbClient } from "../db";
+
+export async function addPerson(firstName, lastName, gtUser, discordUser='', discordId='') {
+    await wrapWithDbClient(async (client) => {
+        const db = await client.db('gtmtg-db');
+        const collection = await db.collection('users');
+        const cursor = await collection.find({
+            discord_id: discordId
+        }, {});
+        if ((await cursor.toArray()).length === 0) {
+            await collection.insertOne({
+                first: firstName,
+                last: lastName,
+                gt_user: gtUser,
+                discord_user: discordUser,
+                discord_id: discordId,
+            });
+            return true;
+        }
+        return false;
+    });
+}
