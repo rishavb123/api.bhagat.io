89a810833cca8b4fb95372fa7971c497b9a5ffd3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-12-07 23:41:56 -0500

    fixed patch for moxfield class name updates

--- server/src/modules/mtg/decks.js
+++ server/src/modules/mtg/decks.js
@@ -14,18 +14,21 @@ export async function getDeckListInfo(url, caching) {
         case 'moxfield':
             const mName = await $eval(page, '.deckheader-name', (el) => el.innerText);
             const mType = await $eval(page, '.badge', (el) => el.innerText);
-            const mDescription = await $eval(page, '.font-weight-light',
+            const mDescription = await $eval(page, '.fw-light.mb-3',
                 (el) => !el.classList.contains('d-flex') ? el.innerText : null);
-            const mComment = await $eval(page, '.comment p', (el) => el? el.innerText: null, false);
+            const mComment = await $eval(page, '.comment p', (el) => el ? el.innerText : null, false);
+            console.log(mComment);
             const mCards = await page.$$eval('.deckview .table-deck-row', (elements) => {
                 const returnVal = [];
                 for (const el of elements) {
                     const nameLink = el.querySelector('.text-body');
+                    const c = parseInt(el.querySelector('td.text-end').innerText);
                     returnVal.push({
                         link: nameLink.href,
-                        count: parseInt(el.querySelector('.text-right').innerText),
+                        count: c,
                         name: nameLink.innerText,
                     });
+
                 }
                 return returnVal;
             });
