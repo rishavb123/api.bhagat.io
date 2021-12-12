b619f9ab5ffcb87bf5b81869956b2e481f3ed21e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-12-12 02:08:03 -0500

    added commands infrastructure setup

---
+++ server/src/commands/index.js
@@ -0,0 +1,42 @@
+import jobsCommands from './jobs';
+
+function processCommands(commands) {
+    for (const command of commands) {
+        if (!command.parseArgs) {
+            command.parseArgs = (message) => {
+                try {
+                    return JSON.parse(message);
+                } catch {
+                    return {};
+                }
+            }
+        }
+        if (!command.triggers) {
+            command.triggers = [];
+        }
+        if (command.triggers instanceof String) {
+            command.triggers = [command.triggers];
+        }
+        command.triggers.push(command.name.replaceAll('_', '-'));
+        if (!command.name) {
+            command.name = command.triggers[0].replaceAll('-', '_');
+        }
+    }
+    return commands;
+}
+
+export default processCommands([
+    {
+        name: 'hello_world',
+        triggers: ['test', 'hello', 'hello-world'],
+        run: async (params) => {
+            if (params.name) {
+                return `Hello, ${params.name}!`;
+            }
+            else {
+                return 'Hello, World!'
+            }
+        }
+    },
+    ...jobsCommands,
+]);
---
+++ server/src/commands/jobs/index.js
@@ -0,0 +1,100 @@
+import { queryGraphQL } from '../../graphql/local';
+import jobs from '../../jobs';
+
+export default [
+    {
+        name: 'list_jobs',
+        parseArgs: (_) => ({}),
+        run: async (_) => {
+            let reply = '';
+            const keys = ['name', 'expression', 'disabled', 'runOnStart', 'runInDev', 'currentlyRunning', 'lastExecuted'];
+            const jobsGQL = (await queryGraphQL(`
+                query getJobs {
+                    jobs {
+                        name
+                        expression
+                        disabled
+                        runOnStart
+                        runInDev
+                        currentlyRunning
+                        lastExecuted
+                    }
+                }
+            `)).data.jobs;
+            for (const job of jobsGQL) {
+                reply += `${job.name}(`;
+                for (const key of keys) {
+                    reply += `${key} = ${job[key]}, `;
+                }
+                reply = reply.substring(0, reply.length - 2);
+                reply += ')\n';
+            }
+            return reply;
+        },
+    },
+    {
+        name: 'get_job',
+        parseArgs: (message) => ({ name: message }),
+        run: async ({ name }) => {
+            const result = await queryGraphQL(`
+                query getJob ($name: String!){
+                    job(name: $name) {
+                        name
+                        expression
+                        disabled
+                        runOnStart
+                        runInDev
+                        currentlyRunning
+                        lastExecuted
+                    }
+                }
+                `, {
+                name: name,
+            });
+            if (result.errors) {
+                return `Job ${name} not found.`;
+            }
+            const job = result.data.job;
+            let reply = `${job.name}(`;
+            const keys = ['name', 'expression', 'disabled', 'runOnStart', 'runInDev', 'currentlyRunning', 'lastExecuted']
+            for (const key of keys) {
+                reply += `\t${key} = ${job[key]}, \n`;
+            }
+            reply = reply.substring(0, reply.length - 3) + '\n)';
+            return reply;
+        }
+    },
+    {
+        name: 'run_job',
+        parseArgs: (message) => {
+            if (message.startsWith('_')) {
+                return {
+                    name: message.substring(1),
+                    wait: false,
+                };
+            } else {
+                return {
+                    name: message,
+                    wait: true,
+                };
+            }
+        },
+        run: async ({ name, wait }) => {
+            const job = jobs.find((job) => job.name == name);
+            if (job) {
+                if (job.running) {
+                    return "Job is already running"
+                } else if (wait) {
+                    await job.call();
+                    return "Finished running job"
+                } else {
+                    job.call();
+                    return "Started job in the background"
+                }
+            } else {
+                return "Job not found. Run !list_jobs to list all jobs."
+            }
+        },
+    },
+
+]
